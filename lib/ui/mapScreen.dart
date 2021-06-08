987f95279bde74b22a50c0efcd403dac807c98d1
aya <ayahmsh123@gmail.com>
2021-06-08 18:57:25 +0300

    aya

--- lib/ui/mapScreen.dart
+++ lib/ui/mapScreen.dart
@@ -8,7 +8,6 @@ class map extends StatefulWidget {
   @override
   map_map_State createState() => map_map_State();
 }
-
 class map_map_State extends State<map> {
   Completer<GoogleMapController> _controller = Completer();
   Iterable markers = [];
@@ -17,9 +16,10 @@ class map_map_State extends State<map> {
       markerId: MarkerId(AppConstant.list[index]['id']),
       position: LatLng(AppConstant.list[index]['lat'], AppConstant.list[index]['lon'],),
       infoWindow: InfoWindow(title: AppConstant.list[index]["title"]),
-      icon: BitmapDescriptor.fromAsset("assets/Image/icons8_hospital.ico"),
+      icon: BitmapDescriptor.fromAsset(AppConstant.list[index]['img']),
     );
-  }
+  },
+
   );
   @override
   void initState() {
@@ -34,7 +34,7 @@ class map_map_State extends State<map> {
       home: Scaffold(
         body: GoogleMap(
           mapType: MapType.normal,
-          initialCameraPosition: CameraPosition(target: LatLng(36.20131681847512, 37.13172031346219), zoom: 17),
+          initialCameraPosition: CameraPosition(target: LatLng(36.20131681847512, 37.13172031346219), zoom: 15),
           onMapCreated: (GoogleMapController controller) {
             _controller.complete(controller);
           },
@@ -46,8 +46,34 @@ class map_map_State extends State<map> {
 }
 class AppConstant {
 static List<Map<String, dynamic>> list = [
-  {"title": "مركز حلب الجديدة", "id": "1", "lat": 36.20061676098412, "lon": 37.130867856524134},
-  {"title": "مركز الفرقان", "id": "2", "lat":  36.200131935863126, "lon":  37.132562993786145},
-  {"title": "مركز الجميلية", "id": "3", "lat":  36.202937001577276, "lon":  37.132391356929574},
+  {"title": "مركز حلب الجديدة", "id": "1", "lat": 36.20314478553057, "lon":  37.131468656146595, "img": "assets/Image/icons8_clinic_64.png" },
+  {"title": "مركز الفرقان", "id": "2", "lat":  36.202434836850266, "lon":  37.13425820018407,"img": "assets/Image/icons8_clinic_64.png" },
+  {"title": "مركز المرديان", "id": "3", "lat":  36.20146517469391, "lon":  37.12887227549016,"img": "assets/Image/icons8_clinic_64.png" },
+  {"title": "مركز بستان القصر", "id": "4", "lat":  36.20056481820934,"lon":  37.13138283665096,"img": "assets/Image/icons8_clinic_64.png" },
+  {"title": "مركز الشهباء", "id": "5", "lat":  36.20040897133898, "lon":  37.132391356929574,"img": "assets/Image/icons8_clinic_64.png" },
+  {"title": "مركز الشعار", "id": "6", "lat":  36.201534401528406, "lon":   37.13586750846024,"img": "assets/Image/icons8_clinic_64.png" },
+
+  {"title": "مخبر حلب الجديدة", "id": "7", "lat":  36.19957785074895, "lon":37.131447213136006,"img": "assets/Image/bloodmap.ico" },
+  {"title": "مخبر حلب الجديدة", "id": "7", "lat":  36.19846966775864, "lon":37.1330135739677,"img": "assets/Image/bloodmap.ico" },
+
+  {"title": "صيدلية حلب الجديدة", "id": "7", "lat":  36.203335151575565, "lon":37.126661989595526,"img": "assets/Image/pharmacymap.ico" },
+  {"title": "صيدلية الجميلية", "id": "7", "lat":  36.20506670605941, "lon":37.136361259707485,"img": "assets/Image/pharmacymap.ico"},
+  {"title": "صيدلية النيرب", "id": "7", "lat":  36.20456463708928, "lon":37.132391370629804,"img": "assets/Image/pharmacymap.ico" },
+
+  {"title": "دكتورة روان", "id": "7", "lat": 36.20122235565245, "lon":37.12149071433444,"img": "assets/Image/doctormsap.ico" },
+  {"title": "دكتورة ريم", "id": "7", "lat":  36.19749986380102, "lon": 37.12604010962752,"img": "assets/Image/doctormsap.ico" },
+  {"title": "دكتورة آية", "id": "7", "lat":  36.199491005068225, "lon": 37.123937133488,"img": "assets/Image/doctormsap.ico" },
+
+
+
+
+
+
+
+
+
+
+
+
 ];
 }
